605ddaa4cee021b2191ee1151c7528403f056cc2
Jasper Holton <jasper@holton.io>
2015-05-18 13:37:08 -0800

    New changes

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test{
+  
+  an=-10+ (n-1)*3
+  public static void main(String[] args)
+  {
+    int result = 24;
+    for(int i = 0; i < 500-1; i++)
+      result+=6;
+    System.out.println(result);
+  }
+    
+}
